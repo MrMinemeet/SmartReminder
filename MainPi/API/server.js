d600d482b13c6bccac80417ac35c59e18c575b4f
Alex | MrMinemeet <alex@wtf-my-code.works>
2023-05-25 13:43:04 +0200

    Re-Transfer API
    Receives data in JSON via POST calls to API. 
    This JSON data gets resent to storage via MQTT
    
    Added node_modules to gitignore

---
+++ MainPi/API/server.js
@@ -0,0 +1,36 @@
+const EXPRESS = require('express');
+const MQTT = require('mqtt');
+const CLIENT = MQTT.connect('mqtt://localhost:8883');
+
+const APP = EXPRESS();
+const PORT =  8080;
+
+app.use(express.urlencoded({ extended: false })); // Parse URL-encoded data in request body
+
+
+APP.gepostt('/', (req, res) => {
+	  res.send('Hello World!');
+});
+
+APP.get('/sendImage', (req, res) => {
+	  res.send('sendImg');
+	  CLIENT.publish('sendImage', req.body.value);
+});
+
+APP.get('/addTask', (req, res) => {
+	  res.send('addTask');
+	  CLIENT.publish('addTask', req.body.value);
+});
+
+APP.get('/getTask', (req, res) => {
+	  res.send('getTask');
+	  CLIENT.publish('getTask', req.body.value);
+});
+
+APP.listen(PORT, () => {
+	console.log(`Server listening on port ${PORT}`);
+});
+
+CLIENT.on('error', (error) => {
+	console.error('Error:', error);
+});
