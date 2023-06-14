79f2df2d0a2fba9b34d0afbc57e1da279892b477
Elias Bürger <elias.buerger@gmx.at>
2023-06-14 15:00:58 +0200

    adfdfdf

--- MainPi/MainStuff/main.py
+++ MainPi/MainStuff/main.py
@@ -88,7 +88,9 @@ def try_detect_tasks_for_person():
         snap = np.empty((camera.resolution[1], camera.resolution[0], 3), dtype=np.uint8)
         camera.capture(snap, format='rgb') # TODO optimize image (rescale, ...)
         encoding = face_recognition.face_encodings(
-            snap)[0]  # TODO no clue if this is working
+            snap)
+        print(encoding)
+        encoding = encoding[0]  # TODO no clue if this is working
         results = face_recognition.compare_faces(images, encoding)
         if len(results) > 0:
             print("Face found")
