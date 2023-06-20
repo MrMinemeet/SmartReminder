3de940067d7ea84006bb858ded091f240085242d
Alexander Voglsperger <alex@wtf-my-code.works>
2023-06-20 20:38:24 +0200

    Using both sensors
    Code by Julia

--- SensorPi/read_sensor_data.py
+++ SensorPi/read_sensor_data.py
@@ -7,25 +7,16 @@ import paho.mqtt.client as mqtt
 def on_connect(client, userdata, flags, rc):
     print("Connected: " + str(rc))
 
-def sensorCallback(channelHall, channelPressure):
+def sensorCallback(client,channelHall, channelPressure):
   timestamp = time.time()
   stamp = datetime.datetime.fromtimestamp(timestamp).strftime('%H:%M:%S')
 
   if GPIO.input(channelPressure):
       start_zeitpunkt = time.time()
       while time.time() - start_zeitpunkt < 20:
-          if(not GPIO.input(channelPressure)):
+          if(not GPIO.input(channelHall)):
               # Open door
               print("Person opened the door " + stamp)
-
-              broker = "192.168.18.16"
-              port = 1883
-
-              client = mqtt.Client()
-
-              # create connection
-              client.on_connect = on_connect
-              client.connect(broker, port, 60)
 
               # send message to broker
               client.publish("door", "Person opened the door")
@@ -35,14 +26,24 @@ def sensorCallback(channelHall, channelPressure):
 
 def main():
   try:
+    broker = "main.local"
+    port = 1883
+
+    # create connection
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.connect(broker, port, 60)
+
     # Loop until users quits with CTRL-C
     while True :
       time.sleep(0.1)
-      sensorCallback(23,17)
+      sensorCallback(client,23,17)
   except KeyboardInterrupt:
     # Reset GPIO settings
     GPIO.cleanup()
-
+  except Exception as e:
+    print(str(e))
+    GPIO.cleanup()
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(23 , GPIO.IN) # hall effect sensor
